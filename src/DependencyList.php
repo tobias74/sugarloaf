f333317f523ad40f37f8c1c7e9daaa97b61d0dde
root <root@AB9EFA7>
2019-07-16 10:32:21 +0000

    remove unused methods, simplify usage

--- src/DependencyList.php
+++ src/DependencyList.php
@@ -4,86 +4,69 @@ namespace SugarLoaf;
 
 class DependencyList
 {
-	public function __construct()
-	{
-		$this->propertyArray = array();
-		$this->parameterArray = new ParameterArray();
-		$this->callbacks = array();
-	}
-	
-	public function addCallback($callback)
-	{
-		$this->callbacks[] = $callback;
-	}
-	
-	public function addDependency($interfaceName, $component)
-	{
-		$component->setInterfaceName($interfaceName);
-		array_push($this->propertyArray, $component);
-		return $this;
-	}
-	
-	public function addManagedDependency($interfaceName, $serviceName=false)
-	{
-		if ($serviceName===false)
-		{
-			$serviceName = $interfaceName;
-		}
-		$component = new \SugarLoaf\Dependency\ManagedDependency($serviceName);
-		return $this->addDependency($interfaceName, $component);
-	}
+    public function __construct()
+    {
+        $this->propertyArray = array();
+        $this->parameterArray = new ParameterArray();
+        $this->callbacks = array();
+    }
 
-	public function addUnmanagedInstance($interfaceName, $instance)
-	{
-		$component = new \SugarLoaf\Dependency\UnmanagedInstance($instance);
-		return $this->addDependency($interfaceName, $component);
-	}
-	
-	
+    public function addCallback($callback)
+    {
+        $this->callbacks[] = $callback;
+    }
 
+    public function addDependency($interfaceName, $component)
+    {
+        $component->setInterfaceName($interfaceName);
+        array_push($this->propertyArray, $component);
 
-	public function appendManagedParameter($parameter)
-	{
-		$this->parameterArray->appendManagedParameter($parameter);
-		return $this;
-	}
+        return $this;
+    }
 
-	public function appendUnmanagedParameter($parameter)
-	{
-		$this->parameterArray->appendUnmanagedParameter($parameter);
-		return $this;
-	}
+    public function addManagedDependency($interfaceName, $serviceName = false)
+    {
+        if (false === $serviceName) {
+            $serviceName = $interfaceName;
+        }
+        $component = new \SugarLoaf\Dependency\ManagedDependency($serviceName);
 
+        return $this->addDependency($interfaceName, $component);
+    }
 
-	public function appendManagedParameterWithName($name, $parameter)
-	{
-		$this->parameterArray->appendManagedParameterWithName($name, $parameter);
-		return $this;
-	}
+    public function addUnmanagedInstance($interfaceName, $instance)
+    {
+        $component = new \SugarLoaf\Dependency\UnmanagedInstance($instance);
 
-	public function appendUnmanagedParameterWithName($name, $parameter)
-	{
-		$this->parameterArray->appendUnmanagedParameterWithName($name, $parameter);
-		return $this;
-	}
+        return $this->addDependency($interfaceName, $component);
+    }
 
+    public function appendManagedParameter($parameter)
+    {
+        $this->parameterArray->appendManagedParameter($parameter);
 
-	public function getPropertyList()
-	{
-		return $this->propertyArray;
-	}
-	
-	public function getParameterArray()
-	{
-		return $this->parameterArray;
-	}
-	
-	public function getCallbacks()
-	{
-		return $this->callbacks;
-	}
-	
-	
+        return $this;
+    }
+
+    public function appendUnmanagedParameter($parameter)
+    {
+        $this->parameterArray->appendUnmanagedParameter($parameter);
+
+        return $this;
+    }
+
+    public function getPropertyList()
+    {
+        return $this->propertyArray;
+    }
+
+    public function getParameterArray()
+    {
+        return $this->parameterArray;
+    }
+
+    public function getCallbacks()
+    {
+        return $this->callbacks;
+    }
 }
-
-
--- src/ParameterArray.php
+++ src/ParameterArray.php
@@ -1,78 +1,54 @@
-<?php 
-namespace SugarLoaf;
+<?php
 
+namespace SugarLoaf;
 
 class ParameterArray
 {
-	public function __construct()
-	{
-		$this->_parameters = array();
-	}
-	
-	public function appendManagedParameter($parameter)
-	{
-		$this->appendParameter(new \SugarLoaf\Dependency\ManagedDependency($parameter));
-		return $this;
-	}
+    public function __construct()
+    {
+        $this->_parameters = array();
+    }
 
-	public function appendUnmanagedParameter($parameter)
-	{
-		$this->appendParameter(new \SugarLoaf\Dependency\UnmanagedInstance($parameter));
-		return $this;
-	}
+    public function appendManagedParameter($parameter)
+    {
+        $this->appendParameter(new \SugarLoaf\Dependency\ManagedDependency($parameter));
 
-	public function appendManagedParameterWithName($name, $parameter)
-	{
-		$this->appendNamedParameter($name, new \SugarLoaf\Dependency\ManagedDependency($parameter));
-		return $this;
-	}
+        return $this;
+    }
 
-	public function appendUnmanagedParameterWithName($name, $parameter)
-	{
-		$this->appendNamedParameter($name, new \SugarLoaf\Dependency\UnmanagedInstance($parameter));
-		return $this;
-	}
+    public function appendUnmanagedParameter($parameter)
+    {
+        $this->appendParameter(new \SugarLoaf\Dependency\UnmanagedInstance($parameter));
 
+        return $this;
+    }
 
+    public function appendParameter($parameter)
+    {
+        $this->_parameters[] = $parameter;
 
+        return $this;
+    }
 
-	public function appendParameter($parameter)
-	{
-		$this->_parameters[] = $parameter;
-		return $this;
-	}
+    public function appendNamedParameter($name, $parameter)
+    {
+        $this->_parameters[$name] = $parameter;
 
+        return $this;
+    }
 
-	public function appendNamedParameter($name, $parameter)
-	{
-		$this->_parameters[$name] = $parameter;
-		return $this;
-	}
+    public function isEmpty()
+    {
+        return 0 === count($this->_parameters);
+    }
 
+    public function getInstance($manager)
+    {
+        $output = array();
+        foreach ($this->_parameters as $index => $parameter) {
+            $output[$index] = $parameter->getInstance($manager);
+        }
 
-	public function isEmpty()
-	{
-		return (count($this->_parameters) === 0);
-	}
-
-
-
-
-	
-	public function getInstance($manager)
-	{
-		$output = array();
-		foreach ($this->_parameters as $index => $parameter)
-		{
-			$output[$index] = $parameter->getInstance($manager);
-		}
-		
-		return $output;
-	}
+        return $output;
+    }
 }
-
-
-
-
-
-
